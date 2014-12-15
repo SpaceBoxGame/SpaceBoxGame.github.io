39ba7a586f9c02dcc70c0cc91f5c3b1bf3c51729
SpaceBoxGame <spaceboxgame@gmail.com>
2014-12-15 15:15:37 -0600

    Addition of Pages

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+
+	    <link rel="stylesheet" href="style.css" type="text/css">
+    <link rel="shortcut icon" href="images/icon.ico">
+	<title>Space Box</title>
+</head>
+<body>
+	<div id="content">
+		<div id="logo">
+			<h1>Space Box</h1>
+			
+		</div>
+		<div id="spaceCash">
+		<ul id="menu">
+			<li><a href="old">Downloads</a></li>
+		</ul>
+			</div>
+		
+
+		<div id="left">
+			<h2>About</h2>
+			Space Box is a WIP incremental game set in space.<br>
+			Space Box contains(will anyway) a handful of features to set it apart from other 
+			incremental games, such as an economy that varies based on where you are in the universe.
+		</div>
+	
+			<div id="right">
+			<h2>Version d12.09.01</h2>
+			<form action="http://www.mediafire.com/download/i06y16qbuc2bbez/Public+Test+Version+d12.09.01.zip">
+  			<center>
+			<input type="image" src="images/downloadButton.png" alt="Download">
+			<p><a href="/spacebox/changelog">changelog</a></p>
+			</center>
+			</form>
+			Mediafire is used to keep my server clean, and providing a backlog of old versions.
+
+			<div style="clear: both"></div>
+		</div>
+
+	</div>
+</body>
+</html>
---
+++ old.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+
+	    <link rel="stylesheet" href="style.css" type="text/css">
+    <link rel="shortcut icon" href="images/icon.ico">
+	<title>Space Box</title>
+</head>
+<body>
+	<div id="content">
+		<div id="logo">
+			<h1>Space Box Versions</h1>
+			
+		</div>
+		<div id="spaceCash">
+		<ul id="menu">
+			<li><a href="old">Downloads</a></li>
+		</ul>
+			</div>
+	
+			<div id="right">
+			<h2>d10.30.4</h2>
+			<a href="http://www.mediafire.com/download/wdm47aaqmurs51j/Public+Test+Version+d10.30.4.zip">Download d10.30.4</a>
+			<br><br>
+			<h2>d10.30.5</h2>
+			<a href="http://www.mediafire.com/download/w14n17m2b5nmbwm/Public+Test+Version+d10.30.5.zip">Download d10.30.5</a>
+			<br><br>
+			<h2>d11.05.01</h2>
+			<a href="http://www.mediafire.com/download/yc56v3htypy03tz/Public+Test+Version+d11.05.01.zip">Download d11.05.01</a>
+			<br><br>
+			<h2>d11.05.02</h2>
+			<a href="http://www.mediafire.com/download/g4hqsi5xdcsqbpt/Public+Test+Version+d11.05.02.zip">Download d11.05.02</a>
+			<br><br>
+			<h2>d11.14.01</h2>
+			<a href="http://www.mediafire.com/download/idcbrd2ix4a36dh/Public+Test+Version+d11.14.01.zip"Download d11.14.01</h2>
+			<br><br>
+			<div style="clear: both"></div>
+		</div>
+
+	</div>
+</body>
+</html>
